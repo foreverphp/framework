b93797a5b508b9820e9811c95e3f8e74733c017d
Daniel Nuñez S <dnunezse@gmail.com>
2017-08-09 22:18:18 -0400

    Ahora los parámetros en SQLSRV se pasan por referencia

--- src/ForeverPHP/Database/SQLEngines/SQLSRVEngine.php
+++ src/ForeverPHP/Database/SQLEngines/SQLSRVEngine.php
@@ -84,7 +84,7 @@ class SQLSRVEngine extends SQLEngine implements SQLEngineInterface {
             $params = array();
 
             foreach ($this->parameters as $param => $paramContent) {
-                $params[] = $paramContent['value'];
+                $params[] = &$paramContent['value'];
             }
 
             // Preparo la consulta

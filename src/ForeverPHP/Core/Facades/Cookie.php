54f6c0b6c8cd09b032cc06b74d8e516d404903d6
Daniel Nuñez <dnunezse@gmail.com>
2025-08-24 04:30:14 -0400

    Fix facades

--- src/ForeverPHP/Core/Facades/Cookie.php
+++ src/ForeverPHP/Core/Facades/Cookie.php
@@ -4,13 +4,10 @@ namespace ForeverPHP\Core\Facades;
 
 /**
  * @method static bool exists(string $name)
- * @method static void set(string $name, string $value, int $expire = 0, string $path = null, string $domain = null,
- *          bool $secure = false, bool $httpOnly = false)
- * @method static void forever(string $name, string $value, string $path = null, string $domain = null,
- *          bool $secure = false, bool $httpOnly = false)
+ * @method static void set(string $name, string $value, int $expire = 0, string $path = null, string $domain = null, bool $secure = false, bool $httpOnly = false)
+ * @method static void forever(string $name, string $value, string $path = null, string $domain = null, bool $secure = false, bool $httpOnly = false)
  * @method static mixed get(string $name)
- * @method static void remove(string $name, string $path = null, string $domain = null, bool $secure = false,
- *          bool $httpOnly = false)
+ * @method static void remove(string $name, string $path = null, string $domain = null, bool $secure = false, bool $httpOnly = false)
  *
  * @see \ForeverPHP\Core\Cookie
  */
